4a3a3cbdb6d019a123898edbccc7f0d32d15a758
alejo <alejo.perez2905@gmail.com>
2025-05-24 16:14:33 -0500

    init proyect chat app

---
+++ src/firebase..js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+       apiKey: "AIzaSyCME2Lq3pYP1bVH52cZEPaNfmEaptcHm1E",
+       authDomain: "chat-react-aa4aa.firebaseapp.com",
+       projectId: "chat-react-aa4aa",
+       storageBucket: "chat-react-aa4aa.firebasestorage.app",
+       messagingSenderId: "500770504673",
+       appId: "1:500770504673:web:2f8814138256004b9d9ca0"
+};
+
+const app = initializeApp(firebaseConfig);
